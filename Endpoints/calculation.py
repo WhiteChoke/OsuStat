72febd0fad56fad7d17a933cbe853b7bc3a9d003
Vealke ⠀ <koristaosu1337@gmail.com>
2026-02-12 14:31:40 +0400

    reinitialized api stuff

---
+++ Endpoints/calculation.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/pp-calculate", tags=["🌟 GET"])
---
+++ api.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+from fastapi import FastAPI
+from Endpoints import calculation
+
+app = FastAPI()
+
+app.include_router(calculation.router)
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", port=727, reload=True)
