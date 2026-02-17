93f7a9b221e9cef1de3a5a45d08bd77ecb1b8c86
WhiteChoke <denisduchenkoo@gmail.com>
2026-02-17 18:59:02 +0900

    created data templates

--- OsuStat.UI/Service/Impl/NavigationService.cs
+++ OsuStat.UI/Service/Impl/NavigationService.cs
@@ -10,7 +10,7 @@ namespace OsuStat.UI.Service.Impl
         public ViewModel CurrentView
         { 
             get => _currentView;
-            set
+            private set
             {
                 _currentView = value;
                 OnPropertyChanged();
