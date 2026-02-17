9f3c2a7e79d08f06aac37631e232b58f43216833
WhiteChoke <denisduchenkoo@gmail.com>
2026-02-17 19:09:15 +0900

    configurated di container

--- OsuStat.UI/App.xaml.cs
+++ OsuStat.UI/App.xaml.cs
@@ -1,14 +1,43 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using OsuStat.UI.MVVM.Core;
+using OsuStat.UI.MVVM.View;
+using OsuStat.UI.MVVM.ViewModel;
 
 namespace OsuStat.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
+            });
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<HomeView>();
+            services.AddSingleton<SettingsView>();
+            
+            services.AddSingleton<IServiceCollection, ServiceCollection>();
+            
+            services.AddSingleton<Func<Type, ViewModel>>
+            (
+                provider => viewModelType => (ViewModel)provider.GetRequiredService(viewModelType)
+            );
+            
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 
 }
