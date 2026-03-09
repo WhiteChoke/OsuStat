576ae005bd2c2fda46c1c74dd6e0e87a763467aa
Vealke ⠀ <koristaosu1337@gmail.com>
2026-03-09 10:43:58 +0400

    .exe file and api file are both fixed

--- Endpoints/api.py
+++ Endpoints/api.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(calculation.router)
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=727, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=727)
