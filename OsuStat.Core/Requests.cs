35682917a61ce052873ff153e91c658b37b91f06
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-25 21:49:27 +0900

    get avatar method moved to Requests class

--- OsuStat.Core/Requests.cs
+++ OsuStat.Core/Requests.cs
@@ -3,9 +3,16 @@
 public static class Requests
 {
     private static readonly HttpClient HttpClient = new();
+    private const string Url = "https://a.ppy.sh/";
+
     
     public static async Task GetUserScores(long replayDataOnlineScoreId)
     {
         throw new NotImplementedException();
+    }
+    
+    public static async Task<byte[]> GetAvatar(int id)
+    {
+        return await HttpClient.GetByteArrayAsync(Url + id);
     }
 }
