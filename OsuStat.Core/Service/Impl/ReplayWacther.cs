034115aadfdd340e37bcb4c2e9be6e9546ee4e53
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-25 21:52:24 +0900

    added on replat registered event

--- OsuStat.Core/Service/Impl/ReplayWacther.cs
+++ OsuStat.Core/Service/Impl/ReplayWacther.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using OsuStat.Core.Extractor;
+using OsuStat.Core.Model;
 using OsuStat.Core.Service.Interfaces;
 
 namespace OsuStat.Core.Service.Impl;
@@ -8,10 +9,11 @@ public class ReplayWatcher : IReplayWatcher
 {
     private FileSystemWatcher? _watcher;
     private readonly ILogger<ReplayWatcher> _logger;
-    private string _gameFolder = @"D:\osu!";
-    public event EventHandler? OnReplayRegistered;
+    private readonly string _gameFolder = @"D:\osu!";
+    public event EventHandler<ReplayData>? OnReplayRegistered;
 
-    public ReplayWatcher(ILogger<ReplayWatcher> logger)
+    public ReplayWatcher(
+        ILogger<ReplayWatcher> logger)
     {
         _logger = logger;
     }
@@ -42,44 +44,7 @@ public class ReplayWatcher : IReplayWatcher
     private void ReplayRegistered(object sender, FileSystemEventArgs e)
     {
         var result = ReplayExtractor.Extract(e.FullPath, _gameFolder);
-        OnReplayRegistered?.Invoke(sender, e);
+        OnReplayRegistered?.Invoke(this, result);
         _logger.LogInformation("Beatmap added: {name}", result.Name);
     }
-
-    // private void UpdateData(BeatMap beatmap, double accuracy) {
-    //     _playerStat.Update(beatmap.Bpm, beatmap.PpGained, beatmap.StarRate, accuracy);
-    //
-    //     if (beatmap.PpGained > _bestScore.Pp)
-    //     {
-    //         _bestScore.Pp = beatmap.PpGained;
-    //         _bestScore.MapName = beatmap.Name;
-    //         _bestScore.BgPath = beatmap.BgPath;
-    //     }
-    //     
-    //     if (_beatmaps.Contains(beatmap))
-    //     {
-    //         var oldBeatmap = _beatmaps.First(b => b.Equals(beatmap));
-    //         _beatmaps.Remove(oldBeatmap);
-    //         
-    //         beatmap.PpGained = oldBeatmap.PpGained > beatmap.PpGained
-    //             ? oldBeatmap.PpGained
-    //             : beatmap.PpGained;
-    //         
-    //         beatmap.PlayCount = oldBeatmap.PlayCount + 1;
-    //     }
-    //     else
-    //     {
-    //         beatmap.PlayCount = 1;
-    //     }
-    //     
-    //     _beatmaps.Insert(0, beatmap);
-    // }
-    //
-    // private List<string> GetIconPathList(List<Mods> mods)
-    // {
-    //     return 
-    //         mods.Select(mod => 
-    //             Path.Combine(_settings.ModIconsFolder, $"{mod}.png"))
-    //             .ToList();
-    // }
 }
