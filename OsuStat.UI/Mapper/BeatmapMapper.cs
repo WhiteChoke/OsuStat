de8410115ecf3a51be73672759ca9b853f269059
WhiteChoke <denisduchenkoo@gmail.com>
2026-04-28 11:19:52 +0900

    created mapper for beatmap

--- OsuStat.UI/Mapper/BeatmapMapper.cs
+++ OsuStat.UI/Mapper/BeatmapMapper.cs
@@ -1,6 +1,40 @@
-﻿namespace OsuStat.UI.Mapper;
+﻿using System.IO;
+using MapsterMapper;
+using OsuParsers.Enums;
+using OsuStat.Data.Models;
+using OsuStat.UI.MVVM.Model;
+using OsuStat.UI.Service;
+
+namespace OsuStat.UI.Mapper;
 
 public class BeatmapMapper
 {
+    private readonly IMapper _mapper;
+    private readonly ISettingsService _settings;
     
+    public BeatmapMapper(IMapper mapper, ISettingsService settings)
+    {
+        _mapper = mapper;
+        _settings = settings;
+    }
+    
+    public BeatMap ToBeatMap(PlayEntity play)
+    {
+        var beatmap = _mapper.Map<BeatMap>(play.Beatmap);
+        beatmap.Accuracy = play.Accuracy;
+        beatmap.MaxCombo = play.Combo;
+        beatmap.PpGained = play.PpGained;
+        beatmap.Mods = ConvertMods(play.Mods);
+        
+        return beatmap;
+    }
+    
+    private List<string> ConvertMods(List<Mods> mods)
+    {
+        return 
+            mods.Select(mod => 
+                    Path.Combine(_settings.ModIconsFolder, $"{mod}.png"))
+                .ToList();
+    }
+
 }
