e17f25e5709ea59289b7e8f88f912bb2f663d8b4
Vealkeee <koristaosu1337@gmail.com>
2026-05-04 19:56:41 +0400

    post best play finished

--- API/Endpoints/discord/post/post_best_play.py
+++ API/Endpoints/discord/post/post_best_play.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from Endpoints.db.engine import localSession
 from Endpoints.db.models import user_best_map
@@ -7,13 +7,19 @@ from Endpoints.db.models import user_best_map
 router = APIRouter(prefix="/discord/user", tags=["🌟 POST"])
 
 class MapScore(BaseModel):
-    best_map: str
+    user_id: int
+    map_chars: dict[str, int | float] = Field(max_length=6)
+    map_name: str
+    map_id: int
 
 @router.post("/latest/new/best-score")
 async def post_latest_best_play(Schema: MapScore):
 
     obj = user_best_map(
-        best_map = Schema.best_map
+        user_id = Schema.user_id,
+        map_name = Schema.map_name,
+        map_chars = Schema.map_chars,
+        map_id = Schema.map_id
     )
 
     async with localSession() as session:
