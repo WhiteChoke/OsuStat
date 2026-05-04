97de747bf8c50da3199afdcf882dfe7a60fb5323
Vealkeee <koristaosu1337@gmail.com>
2026-05-04 19:57:44 +0400

    return best play info obj

--- API/Endpoints/discord/get/get_best_play.py
+++ API/Endpoints/discord/get/get_best_play.py
@@ -6,14 +6,19 @@ from Endpoints.db.models import user_best_map
 
 router = APIRouter(prefix="/discord/user", tags=["🌟 GET"])
 
-@router.get("/latest/best-score")
-async def get_latest_best_play():
-    stmt = (
-        select(user_best_map.id).
-        where(user_best_map.id == 1)
-    )
-    async with localSession() as session:
-        result = await session.execute(stmt)
-        final = result.scalar()
-        print(final)
-    return "success"
+@router.get("/latest/best-score/{id}")
+async def get_latest_best_play(id: int):
+        stmt = (
+            select(user_best_map).
+            where(user_best_map.user_id == id)
+        )
+        async with localSession() as session:
+            result = await session.execute(stmt)
+            final = result.scalars().all()
+            value = final[0]
+            obj = {
+                  "name": value.map_name,
+                  "chars": value.map_chars,
+                  "map_id": value.map_id
+            }
+        return obj
