21b257a5215e18f759c1c572c3934dfe22b5520a
Vealkeee <koristaosu1337@gmail.com>
2026-05-04 19:58:19 +0400

    relationship, repr

--- API/Endpoints/db/models.py
+++ API/Endpoints/db/models.py
@@ -1,11 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import BIGINT
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from sqlalchemy import BIGINT, JSON, ForeignKey
 
 class Base(DeclarativeBase):
     pass
 
 class user_latest_info(Base):
     __tablename__ = "user"
+
+    def __repr__(self):
+        return f"id: {self.user_id}"
 
     # Play count,
     # Different maps played,
@@ -14,7 +17,8 @@ class user_latest_info(Base):
     # AVG star rate,
     # AVG acc
 
-    id: Mapped[int] = mapped_column(BIGINT, primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(BIGINT, unique=True)
     play_count: Mapped[int]
     maps_played: Mapped[int]
     raw_pp_gain: Mapped[int]
@@ -22,8 +26,25 @@ class user_latest_info(Base):
     avg_star_rate: Mapped[int]
     avg_accuracy: Mapped[int]
 
-class user_best_map(Base):
-    __tablename__ = "best"
+    user_best: Mapped["user_best_map"] = relationship(back_populates="user_latest")
 
-    id: Mapped[int] = mapped_column(BIGINT, primary_key=True, nullable=False)
-    best_map: Mapped[str] = mapped_column(nullable=None)
+class user_best_map(Base):
+    __tablename__ = "best" 
+    
+    def __repr__(self):
+        return f'"map_name": {self.map_name}, "map_chars": {self.map_chars}'
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(BIGINT, ForeignKey("user.user_id"), unique=True)
+    map_name: Mapped[str] = mapped_column(nullable=False)
+
+    # 1. AR 
+    # 2. CS 
+    # 3. HP 
+    # 4. length
+    # 5. BPM
+    # 6. Star Rate
+    map_chars: Mapped[dict[str, int | float]] = mapped_column(JSON, nullable=False)
+    map_id: Mapped[str] = mapped_column(BIGINT, nullable=False)
+
+    user_latest: Mapped["user_latest_info"] = relationship(back_populates="user_best")
