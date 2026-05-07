c598be362f3351e6d28dcde7622d8cd42afb2d6f
WhiteChoke <denisduchenkoo@gmail.com>
2026-05-07 17:14:53 +0900

    PlayerExtractor: added pp extract method

--- OsuStat.Core/Extractor/PlayerExtractor.cs
+++ OsuStat.Core/Extractor/PlayerExtractor.cs
@@ -1,10 +1,15 @@
-﻿using OsuParsers.Decoders;
+﻿using System.Text.RegularExpressions;
+using OsuMemoryDataProvider;
+using OsuMemoryDataProvider.OsuMemoryModels.Direct;
+using OsuParsers.Decoders;
 using OsuStat.Core.Model;
 
 namespace OsuStat.Core.Extractor;
 
 public static class PlayerExtractor
 {
+    private const short RetryCount = 5;
+    
     public static PlayerData Extract(string osuDirectoryPath)
     {
         var osuDbPath = Path.Combine(osuDirectoryPath, "osu!.db");
@@ -25,5 +30,24 @@ public static class PlayerExtractor
             Ranking = currentPlayer.Rank,
             LastSeenAt = currentPlayer.LastUpdateTime,
         };
+    }
+    
+    public static int ExtractPp(StructuredOsuMemoryReader reader)
+    {
+        short retryCount = 0;
+        BanchoUser banchoUser = new();
+
+        while (retryCount < RetryCount)
+        {
+            if (reader.TryRead(banchoUser))
+            {
+                var rawPp = Regex.Match(banchoUser.UserPpAccLevel, "[\\d,.]+").Value;
+                var pp = rawPp.Replace(",", "");
+                
+                return int.Parse(pp);
+            }
+            retryCount++;
+        }
+        return 0;
     }
 }
