a5b23cb0628b7a39e2e75fa6c2e7b711e5e3761f
Vealkeee <koristaosu1337@gmail.com>
2026-05-11 11:14:31 +0400

    Patched

--- API/main.py
+++ API/main.py
@@ -4,15 +4,18 @@ import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from Endpoints.db.models import Base
-from Endpoints.db.engine import engine
+# All the comments which are not CORS are DB related
+
+# from Endpoints.db.models import Base
+# from Endpoints.db.engine import engine
 from Endpoints.offline import calculation
 from Endpoints.online import last_time_seen, pp_recieved
-from Endpoints.discord.post import post_latest_info, post_best_play
-from Endpoints.discord.get import get_latest_info, get_best_play 
+# from Endpoints.discord.post import post_latest_info, post_best_play
+# from Endpoints.discord.get import get_latest_info, get_best_play 
 
 app = FastAPI()
 
+# To enable CORS later:
 # app.add_middleware(
 #     CORSMiddleware,
 #     origins=["*"]
@@ -21,10 +24,10 @@ app = FastAPI()
 app.include_router(calculation.router)
 app.include_router(last_time_seen.router)
 app.include_router(pp_recieved.router)
-app.include_router(post_best_play.router)
-app.include_router(post_latest_info.router)
-app.include_router(get_best_play.router)
-app.include_router(get_latest_info.router)
+# app.include_router(post_best_play.router)
+# app.include_router(post_latest_info.router)
+# app.include_router(get_best_play.router)
+# app.include_router(get_latest_info.router)
 
 # @app.on_event("startup")
 # async def on_startup():
