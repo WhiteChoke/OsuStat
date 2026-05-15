e3bc61528a25f25b83931f703c713f61734892b4
WhiteChoke <denisduchenkoo@gmail.com>
2026-05-15 23:14:03 +0900

    Created new map card

---
+++ OsuStat.UI/MVVM/View/MapCard.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OsuStat.UI.MVVM.View;
+
+public partial class MapCard : UserControl
+{
+    public MapCard()
+    {
+        InitializeComponent();
+    }
+}
