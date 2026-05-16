7ff2b56df805889dfdaef49ecead483ddfce905a
WhiteChoke <denisduchenkoo@gmail.com>
2026-05-16 22:07:15 +0900

    Created play card view

---
+++ OsuStat.UI/MVVM/View/PlayCard.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OsuStat.UI.MVVM.View;
+
+public partial class PlayCard : UserControl
+{
+    public PlayCard()
+    {
+        InitializeComponent();
+    }
+}
